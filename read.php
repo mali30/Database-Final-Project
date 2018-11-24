369dee50868c9c5e43d36a2ffcdaac551de41d52
mjaved86 <42703189+mjaved86@users.noreply.github.com>
2018-11-24 15:59:20 -0500

    Add files via upload

---
+++ read.php
@@ -0,0 +1,98 @@
+<?php
+// Check if id parameter exists before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM students WHERE id = :id";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            if($stmt->rowCount() == 1){
+                /* Fetch result row as an associative array.*/
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $age = $row["age"];
+                $gender = $row["gender"];
+                $email = $row["email"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    unset($stmt);
+    
+    // Close connection
+    unset($pdo);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>View Student Details</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p class="form-control-static"><?php echo $row["name"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Gender</label>
+                        <p class="form-control-static"><?php echo $row["gender"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Age</label>
+                        <p class="form-control-static"><?php echo $row["age"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <p class="form-control-static"><?php echo $row["email"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Address</label>
+                        <p class="form-control-static"><?php echo $row["address"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
