39bb6b60b0d592dfe1175cea0581f515d17a4515
Mohamed Ali <mali30@student.gsu.edu>
2018-12-01 21:50:12 -0500

    fix update and delete

--- formprocessor.php
+++ formprocessor.php
@@ -1,6 +1,19 @@
-<?php
+<?php 
+session_start();
+
+
+
+// starting a session so we can go back to main page
+
 // Connect to mysql database 
 $mysqli = new mysqli('127.0.0.1','root',"",'newHospitalDB') or die(mysqli_error($mysqli));
+
+$firstname = "";
+$lastname = "";
+$departID = "";
+$special =  "";
+$update = false;
+$id = 0;
 
 // Check if the save button has been pressed
 if(isset($_POST['save'])){
@@ -10,15 +23,75 @@ if(isset($_POST['save'])){
     $departID = $_POST['departmentID'];
     $special =  $_POST['speciality'];
 
+    
+
     // Insert records into database
     $mysqli -> query("INSERT INTO Doctor(doctorFName , doctorLname , idDepartment, specialty)
      VALUES('$firstname', '$lastname', '$departID' , '$special')") or
     die($mysqli->error);
+
+    $_SESSION['message'] = "You have saved a record into the database";
+    $_SESSION['msg_type'] = "success";
+
+    header("location: index.php");
 }
 
-// get is whats passed in the url
+// // get is whats passed in the url
+// if(isset($_GET['delete'])){
+//     $id = $_GET['delete'];
+//     $mysqli->query("DELETE FROM Doctor WHERE id='$id'") or 
+//     die($mysqli->error());
+// }
+
+// This will delete the record from the table based on the id
 if(isset($_GET['delete'])){
     $id = $_GET['delete'];
-    $mysqli->query("DELETE FROM Doctor WHERE id=$id") or 
-    die($mysqli->error());
+    $mysqli->query("DELETE FROM Doctor WHERE idDepartment AND doctorID = '$id'") or die($mysqli->error);
+
+
+ // When you delete a record, show this message
+ $_SESSION['message'] = "You have saved a deleted a record from the database";
+ $_SESSION['msg_type'] = "danger";
+
+    header("location:index.php");
+     
+
+   
+}
+
+if(isset($_GET['edit'])){
+    $update = true;
+    $id = $_GET['edit'];
+    $result = $mysqli->query("SELECT * FROM Doctor WHERE doctorID = '$id' ") or die($mysqli->error);
+
+    if(count($result) == 1){
+        $row = $result->fetch_array();
+        $firstname = $row['fname'];
+        $lastname = $row['lname'];
+        $idDepart = $row['idDepartment'];
+        $special = $row['specialty'];
+        //  echo (var_dump($result));
+
+    }
+}
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $firstname = $_POST['fname'];
+    $lastname = $_POST['lname'];
+    $idDepart = $_POST['idDepartment'];
+    $special = $_POST['specialty'];
+
+    // $mysqli->query("UPDATE Doctor SET doctorFName='$firstname',
+    // doctorLname = '$lastname' , idDepartment = '$idDepart', specialty = '$special'
+    // WHERE idDepartment='$id' ") or die($mysqli->error);
+
+    $mysqli->query("INSERT INTO Doctor(doctorFname, doctorLname, idDepartment, specialty) VALUES
+     ('$firstname', '$lastname', '$idDepart', '$special' ");
+
+
+    $_SESSION['message'] = "Record has been updated";
+    $_SESSION['msg_type'] = "warning";
+
+    header('location: index.php');
 }
--- index.php
+++ index.php
@@ -10,9 +10,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
-  <?php 
-  require_once 'formprocessor.php';
-  ?>
+  <?php require_once 'formprocessor.php';?>
+  
+  <?php
+    if (isset($_SESSION['message'])): ?>
+    <div class = "alert alert-<?=$_SESSION['msg_type'] ?>">
+
+    <?php
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+        ?>
+
+    </div>
+    <?php endif ?>
   <div class = "container">
 
   <?php
@@ -44,7 +54,7 @@
             <?php
     // everything is fetched from db and stored in row
       while($row = $result->fetch_assoc()):
-        var_dump($row);?>
+        //  var_dump($row);?>
 
             <tr>
             <td> <?php echo $row['doctorID']; ?></td>
@@ -55,15 +65,16 @@
             
 
             <td>
-                <a href="index.php?edit=<?php echo $row['id']; ?>"
+                <a href="index.php?edit=<?php echo $row['idDepartment']; ?>"
                     class="btn btn-info">Edit</a>
-                <a href="formprocessor.php?delete=<?php echo $row['id'];?>"
-                    class="btn btn-danger">Delete</a>
+                <a href="formprocessor.php?delete=<?php echo $row['doctorID'];?>"
+                     class="btn btn-danger">Delete</a>
+                    
             </td>
             </tr>
             
             <?php endwhile;
-                var_dump($row);
+                //  var_dump($row);
 
             ?>
       
@@ -86,33 +97,40 @@
     <div class="row justify-content-center">
     <form action="formprocessor.php" method="post">
 
+    <!-- hidden input field for the update -->
+    <input type="hidden" name="id" value="<?php echo $id; ?>">
+
     <div class="form-group">
     <label> First Name</label>
-    <input type="text" name="fname" class="form-control" value="">
+    <input type="text" name="fname" class="form-control" value="<?php echo $firstname;?>" placeholder="Enter First Name">
     </div>
 
     <div label="form-group">
     <label> Last Name</label>
-    <input type="text" name="lname" class="form-control" value="">
+    <input type="text" value="<?php echo $lastname;?>" name="lname" class="form-control" placeholder="Enter Last Name">
     </div>
 
     <div label="form-group">
     <label> Department ID</label>
-    <input type="text" name="departmentID" class="form-control" value="">
+    <input type="text" value="<?php echo $departmentID;?>" name="departmentID" class="form-control" placeholder="Enter DepartmentID">
     </div>
 
     <div label = "form-group">
     <label> Speciality </label>
-    <input type="text" name="speciality" class="form-control" value="">
+    <input type="text" value="<?php echo $speciality;?>" name="speciality" class="form-control" placeholder="Enter Specialty">
     </div>
 
     <div class="form-group">
+    <?php 
+    if($update == true): ?>
+    <button class="btn btn-info" type="submit" name="update">Update</button>
+<?php else: ?>
     <button class="btn btn-primary" type="submit" name="save">Save</button>
+<?php endif; ?>
     </div>
 
     </form>
     </div>
     </div>
-     
   </body>
 </html>
